35f289f0a1559f6d527218e0dd1632b282fdceb8
tristan beaulieu <tristan@syntheticpawn.com>
2025-07-15 15:06:13 -0400

    Initial Commit

---
+++ GarwController/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GarwController
+//
+//  Created by Tristan Beaulieu on 7/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GarwController/GarwControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarwControllerApp.swift
+//  GarwController
+//
+//  Created by Tristan Beaulieu on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GarwControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
